ccee470add00fbb7eedfd0f1302666321ab2a87a
Meet Vyas <meet.server08@gmail.com>
2021-10-21 11:27:43 +0530

    API gateway

---
+++ src/test/java/com/vyasmeet/springmicroservices/apigetway/ApiGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyasmeet.springmicroservices.apigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
